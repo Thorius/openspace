cd233d3ad95dac828113fd23c251b41fdb084c56
Stefan Ivanov <ivanov.st94@gmail.com>
2016-02-13 13:31:27 +0200

    Added ability to go to a specific scene.

---
+++ client/views/scenes/sceneDetails.js
@@ -0,0 +1,11 @@
+Template.sceneDetails.helpers({ 
+    editors: function() { 
+        // TODO:        
+    }, 
+}); 
+
+Template.sceneDetails.events({ 
+    "click .js-go-to-scene": function(event, template) { 
+         // TODO:
+    } 
+}); 
